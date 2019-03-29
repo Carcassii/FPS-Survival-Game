3cd2029e097f2ef844e63608de5d5b02747d4435
Carcassii <48177997+Carcassii@users.noreply.github.com>
2019-03-29 21:23:43 +0100

    First Commit
    Added Unity Project To GitHub

---
+++ FPS survival game/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 5f;            // How long between each spawn.
+
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        int spawnPointX = Random.Range(165, -163);
+        int spawnPointY = Random.Range(0, 3);
+        int spawnPointZ = Random.Range(187, -138);
+        Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
+}
